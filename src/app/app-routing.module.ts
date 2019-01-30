54442f6e781dd0d9405ce3475df5b3cef3fcf590
gustavosinbandera1 <gustavosinbandera1@hotmail.com>
2019-01-30 01:33:56 -0500

    se agrego una dashboard para navegar entre las diferentes opciones, se agrego una ruta basica, se agregaron algunas librerias en el html para el diseno grafico

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { StudentListComponent } from './student-list/student-list.component';
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    children: [
+      { path: '', redirectTo: 'estudiantes', pathMatch: 'full'},
+      { path: 'estudiantes', component: StudentListComponent}
+    ]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { StudentListComponent } from './student-list/student-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    StudentListComponent
   ],
   imports: [
     BrowserModule,
